5053042d6c8531c30f66d3cbdaead03531b9e03f
Nihal T M <nihaltmdev@gmail.com>
2024-06-28 00:30:17 +0530

    Decompressing zip files

---
+++ decompression/src/main.rs
@@ -0,0 +1,28 @@
+use std::fs;
+use std::io;
+
+fn main() {
+    std::process::exit(real_main());
+}
+
+fn real_main() -> i32 {
+    let args: Vec<_> = std::env::args().collect();
+
+    if args.len() < 2 {
+        println!("Usage: {} `filename` ", args[0]);
+        return 1;
+    }
+
+    let fname = std::path::Path::new(&*args[1]);
+    let file = fs::File::open(&fname).unwrap();
+
+    let mut archive = zip::ZipArchive::new(file).unwrap();
+
+    for i in 0..archive.len() {
+        let mut file = archive.by_index(i).unwrap();
+
+        let outpath = match file.enclosed_name() {
+            
+        }
+    }
+}
