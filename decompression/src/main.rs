24d75592d462e58e11ea9f50606daa5ee606e3bd
Nihal T M <nihaltmdev@gmail.com>
2024-06-28 19:03:47 +0530

    Day2 project unzip equivalent in rust

--- decompression/src/main.rs
+++ decompression/src/main.rs
@@ -1,5 +1,6 @@
 use std::fs;
 use std::io;
+use zip;
 
 fn main() {
     std::process::exit(real_main());
@@ -22,7 +23,40 @@ fn real_main() -> i32 {
         let mut file = archive.by_index(i).unwrap();
 
         let outpath = match file.enclosed_name() {
-            
+            Some(path) => path.to_owned(),
+            None => continue,
+        };
+
+        let comment = file.comment();
+        if !comment.is_empty() {
+            println!("File {} comment: {}", i, comment);
+        }
+
+        if(*file.name()).ends_with('/') {
+            println!("File {} extracted to: \"{}\"", i, outpath.display());
+            fs::create_dir_all(&outpath).unwrap();
+        }
+        else {
+            println!("File {} extracted to: \"{}\" ({} bytes)", i, outpath.display(), file.size());
+
+            if let Some(p) = outpath.parent() {
+                if !p.exists() {
+                    fs::create_dir_all(&p).unwrap();
+                }
+            }
+
+            let mut outfile = fs::File::create(&outpath).unwrap();
+            io::copy(&mut file, &mut outfile).unwrap();
+        }
+
+        #[cfg(unix)]
+        {
+            use std::os::unix::fs::PermissionsExt;
+
+            if let Some(mode) = file.unix_mode() {
+                fs::set_permissions(&outpath, PermissionsExt::from_mode(mode)).unwrap();
+            }
         }
     }
+    0
 }
